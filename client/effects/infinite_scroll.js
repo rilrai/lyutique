8b0a03a07dc6c4f5dcc59b2935adf669c6b18079
rilrai <riddler-@ukr.net>
2016-02-22 13:23:15 +0200

    scroll and pages routing

--- client/effects/infinite_scroll.js
+++ client/effects/infinite_scroll.js
@@ -1,3 +1,16 @@
-$(window).scroll(function(event) {
-  console.log("I'm scrolling this");
-});
+Session.set("newsLimit", 3);
+lastScrollTop = 0; 
+
+$(window).scroll(function(event){
+  // test if we are near the bottom of the window
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    var scrollTop = $(this).scrollTop();
+    // test if we are going down
+    if (scrollTop > lastScrollTop){
+     Session.set("newsLimit", Session.get("newsLimit") + 3);
+    }
+
+    lastScrollTop = scrollTop;
+  }
+      
+})
