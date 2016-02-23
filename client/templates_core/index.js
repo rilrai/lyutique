c50f56831c7e889e2977e1e61a0eb73d4dc162bb
rilrai <riddler-@ukr.net>
2016-02-23 16:46:51 +0300

    lots of fixes

--- client/templates_core/index.js
+++ client/templates_core/index.js
@@ -1,7 +1,6 @@
 Template.indexTemplate.helpers({
   news: function() {
     var result = News.find({}, {sort:{created: -1}, limit:Session.get("newsLimit")}).fetch();
-    console.log(result);
     return result
   }
 })
