0d845e8cbc2337df3130805cb7c286949ab32249
rilrai <riddler-@ukr.net>
2016-03-22 15:12:16 +0200

    22.03

--- collections/collections.js
+++ collections/collections.js
@@ -1,25 +1,25 @@
 News = new Mongo.Collection("news");
-News.allow({
-  insert: function () {
-    if (Session.get("security")) return true
-    return false
-  },
-  remove: function () {
-    if (Session.get("security")) return true
-    return false
-  }
-});
+// News.allow({
+//   insert: function () {
+//     if (Session.get("security")) return true
+//     return false
+//   },
+//   remove: function () {
+//     if (Session.get("security")) return true
+//     return false
+//   }
+// });
 
 Goods = new Mongo.Collection("goods");
-Goods.allow({
-  insert: function () {
-    if (Session.get("security")) return true
-    return false
-  },
-  remove: function () {
-    if (Session.get("security")) return true
-    return false
-  }
-});
+// Goods.allow({
+//   insert: function () {
+//     if (Session.get("security")) return true
+//     return false
+//   },
+//   remove: function () {
+//     if (Session.get("security")) return true
+//     return false
+//   }
+// });
 
 
